543a42b9e3422f3ac35f95bf1d39067c5b367745
PDgaming <produnyadehingia12345@gmail.com>
2026-06-22 12:24:49 +0530

    layers class and parameters method

---
+++ layers.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class Dense:
+    def __init__(self, input, output):
+        self.weight = np.random.randn(input, output) * np.sqrt(2 / input)
+        self.bias = np.ones(output) * 0.01
+
+    def forward(self, input):
+        self.input = input
+
+        z = (self.input @ self.weight) + self.bias
+
+        return z
+
+    def backward(self, gradient):
+        batch_size = self.input.shape[0]
+
+        dL_dw = (self.input.T @ gradient) / batch_size
+        dL_db = np.mean(gradient, axis=0)
+
+        self.dw = dL_dw
+        self.db = dL_db
+
+        dL_dx = gradient @ self.weight.T
+
+        return dL_dx
+
+    def step(self, learning_rate):
+        self.weight -= learning_rate * self.dw
+        self.bias -= learning_rate * self.db
+
+    def parameters(self):
+        return {
+            "weight": self.weight,
+            "bias": self.bias,
+        }
