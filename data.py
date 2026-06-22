a7b266e6dca07cc544d678f660eb057723aa5b0f
PDgaming <produnyadehingia12345@gmail.com>
2026-06-22 13:12:03 +0530

    generating training data set with numpy

--- data.py
+++ data.py
@@ -1,28 +1,6 @@
 import numpy as np
 
-data = np.array(
-    [
-        [1.0],
-        [2.0],
-        [3.0],
-        [4.0],
-    ]
-)
-target = np.array(
-    [
-        [5.0],
-        [8.0],
-        [11.0],
-        [14.0],
-    ]
-)
+data = np.arange(-10, 11, dtype=float).reshape(-1, 1)
+target = 3 * data + 2
 
-
-test_data = np.array(
-    [
-        [5.0],
-        [6.0],
-        [7.0],
-        [8.0],
-    ]
-)
+test_data = np.arange(-12.5, 15.5, 0.5).reshape(-1, 1)
